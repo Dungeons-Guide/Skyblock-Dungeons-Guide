87c97ddf0bc35140241ea0d69cb62751a86a65cc
syeyoung <cyong06@naver.com>
2021-02-03 20:05:36 +0900

    auto reparty on quit, not end

--- src/main/java/kr/syeyoung/dungeonsguide/features/impl/boss/FeatureAutoReparty.java
+++ src/main/java/kr/syeyoung/dungeonsguide/features/impl/boss/FeatureAutoReparty.java
@@ -3,6 +3,7 @@ package kr.syeyoung.dungeonsguide.features.impl.boss;
 import kr.syeyoung.dungeonsguide.e;
 import kr.syeyoung.dungeonsguide.features.SimpleFeature;
 import kr.syeyoung.dungeonsguide.features.listener.DungeonEndListener;
+import kr.syeyoung.dungeonsguide.features.listener.DungeonQuitListener;
 import kr.syeyoung.dungeonsguide.features.listener.GuiBackgroundRenderListener;
 import kr.syeyoung.dungeonsguide.utils.TextUtils;
 import net.minecraft.client.Minecraft;
@@ -18,13 +19,13 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import org.lwjgl.opengl.GL11;
 
-public class FeatureAutoReparty extends SimpleFeature implements DungeonEndListener {
+public class FeatureAutoReparty extends SimpleFeature implements DungeonQuitListener {
     public FeatureAutoReparty() {
         super("Bossfight", "Auto reparty when dungeon finishes","Auto reparty on dungeon finish\n\nThis automates player chatting action, (disbanding, repartying) Thus it might be against hypixel's rules.\nBut mods like auto-gg exist so I'm leaving this feature.\nThis option is use-at-your-risk and you'll be responsible for ban if you somehow get banned because of this feature\n(Although it is not likely to happen)\nDefaults to off", "bossfight.autoreparty", false);
     }
 
     @Override
-    public void onDungeonEnd() {
+    public void onDungeonQuit() {
         if (isEnabled()) e.getDungeonsGuide().getCommandReparty().requestReparty();
     }
 }
