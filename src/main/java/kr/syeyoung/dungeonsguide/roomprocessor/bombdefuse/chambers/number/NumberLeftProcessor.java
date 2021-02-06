a615416e4e4e8d63740a4825ed6788ac16e2f151
syeyoung <cyong06@naver.com>
2021-02-06 17:11:14 +0900

    left proc

--- src/main/java/kr/syeyoung/dungeonsguide/roomprocessor/bombdefuse/chambers/number/NumberLeftProcessor.java
+++ src/main/java/kr/syeyoung/dungeonsguide/roomprocessor/bombdefuse/chambers/number/NumberLeftProcessor.java
@@ -83,7 +83,7 @@ public class NumberLeftProcessor extends GeneralDefuseChamberProcessor {
             d2 = (answer % 1000) / 100;
             d3 = (answer % 100) / 10;
             d4 = (answer % 10);
-            answer = d4 * 1000 + d3 * 100 + d2 * 10 + d1;
+            answer = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
         }
     }
 
