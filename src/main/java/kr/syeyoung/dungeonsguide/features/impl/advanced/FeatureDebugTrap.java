63364ba02e59c65322f888ce6d0810f6ab0d7309
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2022-10-28 20:55:24 +0200

    added dummy support in FeatureDebugTrap
    Signed-off-by: kingstefan26 <70776766+kingstefan26@users.noreply.github.com>

--- src/main/java/kr/syeyoung/dungeonsguide/features/impl/advanced/FeatureDebugTrap.java
+++ src/main/java/kr/syeyoung/dungeonsguide/features/impl/advanced/FeatureDebugTrap.java
@@ -16,17 +16,19 @@ import java.util.List;
 
 public class FeatureDebugTrap extends TextHUDFeature {
     public FeatureDebugTrap() {
-        super("Debug", "Display the current amount of bat entities", "", "advanced.bat", false, getFontRenderer().getStringWidth("You're in puzzle-tictactoe"), getFontRenderer().FONT_HEIGHT);
-        getStyles().add(new TextStyle("in", new AColor(0x00, 0xAA,0xAA,255), new AColor(0, 0,0,0), false));
-        getStyles().add(new TextStyle("roomname", new AColor(0x55, 0xFF,0xFF,255), new AColor(0, 0,0,0), false));
+        super("Debug", "Display the current amount of bat entities", "", "advanced.bat", false, getFontRenderer().getStringWidth("Bats: 9999"), getFontRenderer().FONT_HEIGHT);
+        getStyles().add(new TextStyle("base", new AColor(0x00, 0xAA,0xAA,255), new AColor(0, 0,0,0), false));
+        getStyles().add(new TextStyle("batsamm", new AColor(0x55, 0xFF,0xFF,255), new AColor(0, 0,0,0), false));
     }
+    
+    
 
     SkyblockStatus skyblockStatus = DungeonsGuide.getDungeonsGuide().getSkyblockStatus();
 
     private static final List<StyledText> dummyText=  new ArrayList<>();
     static {
-        dummyText.add(new StyledText("You're in ","in"));
-        dummyText.add(new StyledText("puzzle-tictactoe","roomname"));
+        dummyText.add(new StyledText("Bats: ","base"));
+        dummyText.add(new StyledText("9999","batsamm"));
     }
 
 
@@ -53,7 +55,7 @@ public class FeatureDebugTrap extends TextHUDFeature {
 
     @Override
     public List<String> getUsedTextStyle() {
-        return Arrays.asList("roomname", "in");
+        return Arrays.asList("batsamm", "base");
     }
 
     @Override
@@ -68,10 +70,10 @@ public class FeatureDebugTrap extends TextHUDFeature {
 
 
         List<StyledText> actualBit = new ArrayList<>();
-        actualBit.add(new StyledText("Bats: ","in"));
+        actualBit.add(new StyledText("Bats: ","base"));
 
 
-        actualBit.add(new StyledText(String.valueOf(getAvgTimeItTookToPAcket()),"roomname"));
+        actualBit.add(new StyledText(String.valueOf(getAvgTimeItTookToPAcket()),"batsamm"));
 
         return actualBit;
     }
