35d3578a1dbce0af92e68275564e19c583e35acd
StyStatic <97907654+StyStatic@users.noreply.github.com>
2023-09-08 19:02:09 -0700

    I call this "Shut Up"
    Signed-off-by: StyStatic <97907654+StyStatic@users.noreply.github.com>

--- loader/src/main/java/kr/syeyoung/dungeonsguide/launcher/auth/AuthManager.java
+++ loader/src/main/java/kr/syeyoung/dungeonsguide/launcher/auth/AuthManager.java
@@ -153,14 +153,6 @@ public class AuthManager {
                         .build()));
             } else {
                 currentToken = new FailedAuthToken(e);
-                NotificationManager.getInstance().updateNotification(authenticationFailure, new WidgetNotification(authenticationFailure, Notification.builder()
-                        .title("Auth Error")
-                        .description("Authentication Error Occurred\n"+e.getMessage())
-                        .titleColor(0xFFFF0000)
-                        .onClick(() -> {
-                            GuiDisplayer.INSTANCE.displayGui(new GuiScreenAdapter(new GlobalHUDScale(new WidgetError(e))));
-                        })
-                        .build()));
             }
             logger.error("Re-auth failed with message {}, trying again in a 2 seconds", String.valueOf(Throwables.getRootCause(e)));
             throw new AuthFailedException(e);
@@ -206,14 +198,6 @@ public class AuthManager {
                             .build()));
                 } else {
                     currentToken = new FailedAuthToken(e);
-                    NotificationManager.getInstance().updateNotification(authenticationFailure, new WidgetNotification(authenticationFailure, Notification.builder()
-                            .title("Auth Error")
-                            .description("Authentication Error Occurred\n"+e.getMessage())
-                            .titleColor(0xFFFF0000)
-                            .onClick(() -> {
-                                GuiDisplayer.INSTANCE.displayGui(new GuiScreenAdapter(new GlobalHUDScale(new WidgetError(e))));
-                            })
-                            .build()));
                 }
                 logger.error("Accepting the Privacy Policy failed with message {}, trying again in a 2 seconds", String.valueOf(Throwables.getRootCause(e)));
                 throw new AuthFailedException(e);
