879fae36e1509a70bcd26d025b1f6ab889fb80d2
syeyoung <cyoung06@naver.com>
2024-10-13 00:45:51 +0900

    Revert "- remove direct jna dependency / random native dir"
    This reverts commit 2e09a358c7c4d884c5e746ebb103b1507af0822d.

--- mod/src/main/java/kr/syeyoung/dungeonsguide/mod/NativeLoader.java
+++ mod/src/main/java/kr/syeyoung/dungeonsguide/mod/NativeLoader.java
@@ -31,21 +31,22 @@ public class NativeLoader {
 
     public static void extractLibraryAndLoad(String name) throws IOException {
         String libName = System.mapLibraryName(name);
-        String dir;
-        if (Platform.isARM()) {
-            dir = "aarch64";
-        } else if (Platform.is64Bit()){
-            dir = "x86_64";
-        } else {
-            dir = "x86";
+        String dir = Platform.ARCH;
+
+        if (!(dir.equals("aarch64") || dir.equals("x86") || dir.equals("x86_64"))) {
+            if (Platform.is64Bit()) {
+                dir = "x86_64";
+            } else {
+                dir = "x86";
+            }
         }
 
         String resourceLoc = "/native/"+dir+"/"+libName;
 
         System.out.println("Extracting "+name+" from "+resourceLoc);
-        System.out.println("Arch: "+ dir +" | OS: "+Platform.getOSType());
+        System.out.println("Arch: "+Platform.ARCH +" | OS: "+Platform.getOSType());
 
-        File targetExtractionPath = new File("native-"+(System.nanoTime() % 1000000)+"/"+libName);
+        File targetExtractionPath = new File("native/"+libName);
         targetExtractionPath.getParentFile().mkdirs();
         try (InputStream is = NativeLoader.class.getResourceAsStream(resourceLoc)) {
             Files.copy(is, targetExtractionPath.toPath(), StandardCopyOption.REPLACE_EXISTING);
