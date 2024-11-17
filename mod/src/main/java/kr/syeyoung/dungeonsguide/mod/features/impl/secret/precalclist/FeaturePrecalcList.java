e7da189b993109beb258bc85ea080b840cc65ecc
syeyoung <cyoung06@naver.com>
2024-11-17 15:19:41 +0900

    - precalc list
    Signed-off-by: syeyoung <cyoung06@naver.com>

---
+++ mod/src/main/java/kr/syeyoung/dungeonsguide/mod/features/impl/secret/precalclist/FeaturePrecalcList.java
@@ -0,0 +1,25 @@
+package kr.syeyoung.dungeonsguide.mod.features.impl.secret.precalclist;
+
+import kr.syeyoung.dungeonsguide.mod.features.SimpleFeature;
+import kr.syeyoung.dungeonsguide.mod.features.impl.secret.pfrequest.WidgetCheckMissing;
+import kr.syeyoung.dungeonsguide.mod.features.impl.secret.pfrequest.WidgetRequestCalculation;
+import kr.syeyoung.dungeonsguide.mod.guiv2.Widget;
+
+import java.util.List;
+
+public class FeaturePrecalcList extends SimpleFeature {
+    public FeaturePrecalcList() {
+        super("Pathfinding & Secrets", "Precalculations", "Browse Precalculations", "secret.precalclist");
+        setEnabled(true);
+    }
+
+    @Override
+    public boolean isDisableable() {
+        return false;
+    }
+
+    @Override
+    public void setupConfigureWidget(List<Widget> widgets) {
+        super.setupConfigureWidget(widgets);
+    }
+}
---
+++ mod/src/main/java/kr/syeyoung/dungeonsguide/mod/features/impl/secret/precalclist/WidgetPrecalcList.java
@@ -0,0 +1,10 @@
+package kr.syeyoung.dungeonsguide.mod.features.impl.secret.precalclist;
+
+import kr.syeyoung.dungeonsguide.mod.guiv2.xml.AnnotatedImportOnlyWidget;
+import net.minecraft.util.ResourceLocation;
+
+public class WidgetPrecalcList extends AnnotatedImportOnlyWidget {
+    public WidgetPrecalcList() {
+        super(new ResourceLocation("dungeonsguide:gui/features/precalclist/"));
+    }
+}
