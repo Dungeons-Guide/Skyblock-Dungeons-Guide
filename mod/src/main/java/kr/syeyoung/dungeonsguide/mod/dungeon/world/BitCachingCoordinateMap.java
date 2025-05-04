d3dd7a7d4ab481f6460debb9f4da8a1f880bd6d4
syeyoung <cyoung06@naver.com>
2025-05-05 00:58:18 +0900

    - fix cache invalidation
    Signed-off-by: syeyoung <cyoung06@naver.com>

--- mod/src/main/java/kr/syeyoung/dungeonsguide/mod/dungeon/world/BitCachingCoordinateMap.java
+++ mod/src/main/java/kr/syeyoung/dungeonsguide/mod/dungeon/world/BitCachingCoordinateMap.java
@@ -61,7 +61,7 @@ public class BitCachingCoordinateMap<T extends Enum<T>> implements ICoordinateMa
 
     public void invalidateCache(int x, int y, int z) {
         if (x < minX || z < minZ || x >= maxX || z >= maxZ || y < minY || y >= maxY) return;
-        bitStorage.store(x, y, z, 0);
+        bitStorage.store(x - minX, y - minY, z - minZ, 0);
     }
 
     @Override
